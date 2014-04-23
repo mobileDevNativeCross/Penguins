63961f2c9d9b1050e388e861dc21b05f93ff2882
darkprogrammer <drudeveldvurech@gmail.com>
2014-04-23 15:18:51 +0300

    Penguins Seal Gameplay bear background Xcode catapult

---
+++ Penguines.spritebuilder/Source/Gameplay.h
@@ -0,0 +1,13 @@
+//
+//  Gameplay.h
+//  Penguines
+//
+//  Created by Mac on 23.04.14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCNode.h"
+
+@interface Gameplay : CCNode
+
+@end
