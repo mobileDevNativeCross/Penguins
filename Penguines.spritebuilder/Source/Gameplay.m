c036b3cd99c9b5f675c6056ed67a760fb967cf4e
darkprogrammer <drudeveldvurech@gmail.com>
2014-04-23 16:34:55 +0300

    retry flying

--- Penguines.spritebuilder/Source/Gameplay.m
+++ Penguines.spritebuilder/Source/Gameplay.m
@@ -12,6 +12,7 @@
     CCPhysicsNode *_physicsNode;
     CCNode *_catapultArm;
     CCNode *_levelNode;
+    CCNode *_contentNode;
 }
 // is called when CCB file has completed loading
 - (void)didLoadFromCCB {
@@ -31,7 +32,7 @@
     CCNode* penguin = [CCBReader load:@"Penguin"];
     // position the penguin at the bowl of the catapult
     penguin.position = ccpAdd(_catapultArm.position, ccp(16, 50));
-    
+  //  _contentNode.position = ccpAdd(_catapultArm.position, ccp(16, 50));
     // add the penguin to the physicsNode of this scene (because it has physics enabled)
     [_physicsNode addChild:penguin];
     
@@ -39,12 +40,18 @@
     CGPoint launchDirection = ccp(1, 0);
     CGPoint force = ccpMult(launchDirection, 6000);
     [penguin.physicsBody applyForce:force];
+   // [_contentNode.physicsBody applyForce:force];
     
     
     // ensure followed object is in visible are when starting
     self.position = ccp(0, 0);
     CCActionFollow *follow = [CCActionFollow actionWithTarget:penguin worldBoundary:self.boundingBox];
-    [self runAction:follow];
+    [_contentNode runAction:follow];
+}
+
+- (void)retry {
+    // reload this level
+    [[CCDirector sharedDirector] replaceScene: [CCBReader loadAsScene:@"Gameplay"]];
 }
 
 @end
