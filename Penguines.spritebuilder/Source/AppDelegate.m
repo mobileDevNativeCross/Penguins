9591c67b98b2d9ee91cab247e4c57edf625829e2
darkprogrammer <drudeveldvurech@gmail.com>
2014-04-23 14:48:38 +0300

    Penguins Seal button

--- Penguines.spritebuilder/Source/AppDelegate.m
+++ Penguines.spritebuilder/Source/AppDelegate.m
@@ -53,8 +53,8 @@
     //[cocos2dSetup setObject:kEAGLColorFormatRGB565 forKey:CCConfigPixelFormat];
     
     [self setupCocos2dWithOptions:cocos2dSetup];
-    [CCBReader load:@"Penguin"];
-    [CCBReader load:@"Seal"];
+   // [CCBReader load:@"Penguin"];
+    //[CCBReader load:@"Seal"];
     
     return YES;
 }
